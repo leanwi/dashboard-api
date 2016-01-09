322257c392890a05100bc9c3290adfee6d3f7887
Gus Falkenberg <gus@gkf.name>
2016-01-09 07:53:06 -0600

    Fixed date to work with mongodb driver

--- middlewares/fixDates.js
+++ middlewares/fixDates.js
@@ -2,10 +2,10 @@ var moment = require('moment');
 
 module.exports = function(req, res, next) {
   if(req.params.start) {
-    res.locals.start = moment(req.params.start, 'MM-DD-YYYY').startOf('day').format('YYYY-MM-DD HH:mm:ss');
+    res.locals.start = moment(req.params.start, 'MM-DD-YYYY').startOf('day').toDate();
   }
   if(req.params.end) {
-    res.locals.end = moment(req.params.end, 'MM-DD-YYYY').endOf('day').format('YYYY-MM-DD HH:mm:ss');
+    res.locals.end = moment(req.params.end, 'MM-DD-YYYY').endOf('day').toDate();
   }
 
   next();
